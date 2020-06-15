27ea10169c9bfe5238410730326a65d422bd4f70
Umair Shahid <umair.shahid0088@gmail.com>
2020-06-15 21:57:14 +0500

    complete all tasks

---
+++ app.js
@@ -0,0 +1,326 @@
+
+
+
+// chaper 1 tasks
+//task 1
+// alert("ERROR! Please Enter a valid password");
+
+//task 2
+// alert("Welcome to js land... \n Happy coding!");
+
+//task 3
+// alert("Welcome to js land...");
+// alert("Happy coding ");
+
+//compete all chapter 3 tasks
+
+
+
+
+//Chapter 2 Tasks
+//task 1
+// var myName="Umair shahid";
+
+///task 2
+// var message="Hello world"
+// alert(message);
+
+// task 3
+// var name="umair shahid"
+// var age="15 year old"
+// var course="Certified Mobile and web development"
+// alert(name)
+// alert(age)
+// alert(course)
+
+// task 4
+// var email="umair.shahid0088@gmail.com"
+// alert("This is my email "+email)
+
+// task 5
+// document.write("yeah i can write through javascript")
+
+
+
+
+//Chapter 3
+//task 1
+// var age=24
+// document.write("my age is "+age)
+
+//task 2
+// var birthYear=1995
+// document.write("your birth year is "+birthYear)
+// document.write("data type of my variable is number")
+
+// task 4
+// var visiterName="asad"
+// var productTittle="T-shirts"
+// var quantity="5"
+// document.write(visiterName+" ordered "+quantity+" "+productTittle+" on XYZ clothing store")
+
+
+
+//Chapter 5
+// //task 1
+// var n1=Number(prompt("enter numbber 1"))
+// var n2=Number(prompt("enter numbber 2"))
+// var result=n1+n2
+// document.write("sum of "+n1+" and "+n2+" is "+result)
+
+// task 2
+// var n1=Number(prompt("enter numbber 1"))
+// var n2=Number(prompt("enter numbber 2"))
+// var result=n1+n2
+// document.write("sum of "+n1+" and "+n2+" is "+result)
+
+// //task 3
+// result=n1-n2
+// document.write("Subtraction of "+n1+" and "+n2+" is "+result)
+
+// result=n1*n2
+// document.write("Multiply of "+n1+" and "+n2+" is "+result)
+
+// result=n1/n2
+// document.write("Divide of "+n1+" and "+n2+" is "+result)
+
+// result=n1%n2
+// document.write("Mode of "+n1+" and "+n2+" is "+result)
+
+//task 4
+// var ticketPrice=600
+// var quantity=5
+// var netPrice=ticketPrice*quantity
+// document.write("Total cost of buy "+quantity+" tickets is "+netPrice)
+
+
+// /chapter 6-9 
+// task 1
+// var a=10
+// document.write("the value is "+a+"<br> <br>")
+// a++
+// document.write("the value of a++ is 11 <br>")
+// document.write("now the value of a is "+a+"<br> <br>")
+// a++
+// document.write("the value of a++ is 11 <br>" )
+// document.write("now the value is "+a +"<br> <br>")
+// --a
+// document.write("the value of --a is 11 <br>")
+// document.write("now the value is "+a+"<br> <br>")
+// a--
+// document.write("the value of a-- is 11 <br>")
+// document.write("now the value is "+a+"<br> <br>")
+
+
+// task 2
+// var name=prompt("enter you name")
+// document.write("welcome Mr "+name)
+
+// //task 3
+// var eng=Number(prompt("enter english obtain marks out of 100"))
+// var urdu=Number(prompt("enter urdu obtain marks out of 100"))
+// var math=Number(prompt("enter math obtain marks out of 100"))
+// var totalMarks=100 // each subject total marls is 100
+
+// document.write("Sub  Total marks  Obt marks  per <br>" )
+// document.write("Engish "+totalMarks+" "+eng+" "+(eng*100/totalMarks+"% <br>") )
+// document.write("Urdu "+totalMarks+" "+urdu+" "+(urdu*100/totalMarks+"% <br>") )
+// document.write("Math "+totalMarks+" "+math+" "+(math*100/totalMarks+"% <br>") )
+
+
+
+
+
+// Chapter 9 -11
+// Task 1
+// var city=prompt("Enter city name")
+// if(city==="karachi"){
+// document.write("Welcome to "+city)
+// }
+
+// task 2
+// var gender=prompt("Enter gender")
+// if(gender==="male"){
+// document.write("Good morning sir")
+// }
+// else{
+//     document.write("Good morning Mam")
+// }
+
+// task 3
+// var color=prompt("Enter color")
+// if(color==="red"){
+// document.write("Must Stop")
+// }
+// else if(color==="yellow"){
+//     document.write("Ready to go")
+// }
+// else{
+// document.write("Go now")
+// }
+
+
+
+
+
+//chapter 12-13
+//task 1
+// var n1=prompt("Enter number 1")
+// var n2=prompt("Enter number 2")
+
+// if(n1 > n2 ){
+// document.write("Number 1 is big than numbe 2" )
+// }
+// else if(n2 > n1){
+//     document.write("Number 2 is big than numbe 1" )
+// }
+// else{
+// document.write("Number 1 and Number 2 are equal")
+// }
+
+// // task 2
+// var character=prompt("Enter number 1")
+// if(character==="a" || character==="e" || character==="i" || character==="o" || character==="u"   ){
+// document.write("True, it is a vovel" )
+// }
+// else{
+//     document.write("False, its not a vovel" )
+// }
+
+// task 3
+// var password="test123"
+// var E_password=prompt("Enter your password")
+// if(E_password===password){
+// document.write("correct possword")
+// }
+// else if(E_password===""){
+// document.write("enter your password")
+// }
+// else{
+//     document.write("Incorrect possword")
+// }
+
+//task 4 fix
+// var greeting; 
+// var hour = 13;
+//  if (hour < 18) {
+//       greeting = "Good day"; 
+//  }
+//       else {
+//       greeting = "Good evening"; 
+//     }
+
+
+
+
+
+//chapter 14-16
+// task 1
+// var car=[];
+
+//task 2
+// var car=new Array()
+
+// task 3
+// var stringArry=["ali", "asad", "adnan"];
+
+// task 4
+// var numArray=[2, 4, 7]
+
+// task 5
+// var booleanArray=[true,false,true]
+
+// task 6
+// var MixArray=[2, "asad", 7, "awais"]
+
+// task 7
+// var HECArray=["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"]
+// document.write("1) "+HECArray[0] +"<br>")
+// document.write("2) "+HECArray[1] +"<br>")
+// document.write("3) "+HECArray[2] +"<br>")
+// document.write("4) "+HECArray[3] +"<br>")
+// document.write("5) "+HECArray[4] +"<br>")
+// document.write("6) "+HECArray[5] +"<br>")
+// document.write("7) "+HECArray[6] +"<br>")
+// document.write("8) "+HECArray[7] +"<br>")
+
+// task 8
+// var score=[320, 230, 480, 120]
+// document.write("student score "+score+"<br>")
+// document.write("ordered student score "+score.sort()+"<br>")
+
+// task 9
+// var arr=["this", "is","my" ,"car"]
+// document.write("Array "+ arr +"<br>")
+// document.write("String "+ arr[0]+" "+ arr[1]+" "+ arr[2]+" "+ arr[3]+" ")
+
+
+
+
+
+
+// chapter 17-20
+// task 1
+// var arr=[
+//     [3,5,6]
+//     [2,9,0]
+// ]
+
+// task 2
+// var arr=[
+//     [0,1,2,3]
+//     [1,0,1,2]
+//     [2,1,0,1]
+// ]
+
+// task 3
+// for(var a=1; a<+10;a++){
+// document.write(a+"<br>")
+// }
+
+// // task 4
+// var table=Number(prompt("enter table"))
+// var t_length=Number(prompt("enter table length"))
+
+// for(var a=1; a<=t_length;a++){
+// document.write(table+" X "+a+" = "+table*a+"<br>")
+// }
+
+// // task 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"] 
+// for(var a=0; a<fruits.length; a++){
+// document.write("Element at index "+a+" is "+fruits[a]+"<br>")
+// }
+
+// task 6
+// document.write("Reverse Counting <br>")
+// for(var a=1; a<=10;a++){
+// document.write(a+" ")
+// }
+
+// document.write("<br> Counting <br>")
+
+// for(var a=10; a>=1;a--){
+//     document.write(a+" ")
+//     }
+// document.write("<br> Even Number <br>")
+
+// for(var a=0; a<=10;a+=2){
+//     document.write(a+" ")
+//     }
+// document.write("<br> Odd Number <br>")
+// for(var a=1; a<=10;a+=2){
+//     document.write(a+" ")
+//     }
+
+// task 7
+// var input=prompt("enter name")
+// var arr=["cake", "apple pie", "cookie", "chips", "patties"] 
+// for(var a=0; a<=arr.length; a++){
+// if(input===arr[a]){
+// document.write(arr[a] +" is avaiable at index "+a+" of our bakery")
+// }
+// }
+
+
+
